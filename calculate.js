ca7d4a1c4407b1ba39818dec5cace15558ca2f98
gabydi106 <gabydi106@gmail.com>
2017-03-29 22:42:22 +0800

    new calculate

--- calculate.js
+++ calculate.js
@@ -1,14 +1,24 @@
 //size of box
 //change radio into buttons
 
-<input type="button"  name="selectedcake" value="Round6" />
+
+<form action="" id="cakeform">
+	<input type="button"  name="selectedcake" value="Round6" />
 	Round cake 6" -  serves 8 people ($20)
-<input type="button"  name="selectedcake" value="Round8" />
-	Round cake 8" - serves 12 people ($25)
-<input type="button"  name="selectedcake" value="Round10" />
-	Round cake 10" - serves 16 people($35)
-<input type="button"  name="selectedcake" value="Round12" />
-	Round cake 12" - serves 30 people($75)
+	<input type="button"  name="selectedcake" value="Round8" />
+		Round cake 8" - serves 12 people ($25)
+	<input type="button"  name="selectedcake" value="Round10" />
+		Round cake 10" - serves 16 people($35)
+	<input type="button"  name="selectedcake" value="Round12" />
+		Round cake 12" - serves 30 people($75)
+</form>
+
+
+
+
+//In the javascript code
+var theForm = document.forms["cakeform"];
+
 
 var cake_prices = new Array();
 cake_prices["Round6"]=20;
@@ -29,8 +39,7 @@ function getCakeSizePrice()
 	//We loop through each radio buttons
 	for(var i = 0; i < selectedCake.length; i++)
 	{
-	//if the radio button is checked
-		if(selectedCake[i].checked)
+		if(selectedCake[i].checked) //CHANGE TO if ___ has class 'active' then
 		{
 			//we set cakeSizePrice to the value of the selected radio button
 			//i.e. if the user choose the 8" cake we set it to 25
@@ -47,4 +56,13 @@ function getCakeSizePrice()
 	return cakeSizePrice;
 }
 
-
+function getTotal()
+{
+	//Here we get the total price by calling our function
+	//Each function returns a number so by calling them we add the values they return together
+	var cakePrice = getCakeSizePrice() + getFillingPrice() +
+	candlesPrice() + insciptionPrice();
+	//display the result
+	document.getElementById('totalPrice').innerHTML =
+	"Total Price For Cake $"+cakePrice;
+}
